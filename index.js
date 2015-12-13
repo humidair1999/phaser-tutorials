e6f0a8790dc293421bbd19d82107b51a8a29b991
J. Ky Marsh <jkymarsh@gmail.com>
2015-12-13 15:31:04 -0800

    add basic scene elements and create code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
+app.use('/assets', express.static('public'));
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
